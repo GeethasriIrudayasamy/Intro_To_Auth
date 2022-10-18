339bf10e13949a1c46b18b44fd716220523ca7d0
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-10-18 19:34:36 +0530

    contextAPI

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,16 +1,13 @@
 import React from "react";
-
 import Navigation from "./Navigation";
+
 import classes from "./MainHeader.module.css";
 
 const MainHeader = (props) => {
     return (
         <header className={classes["main-header"]}>
             <h2>Hello There!</h2>
-            <Navigation
-                isLoggedIn={props.isAuthenticated}
-                onLogout={props.onLogout}
-            />
+            <Navigation />
         </header>
     );
 };
